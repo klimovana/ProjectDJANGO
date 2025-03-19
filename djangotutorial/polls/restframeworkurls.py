192df4ee8a4ac88d008f9185371255ee284d12e9
klimovana <a2r_k@mail.ru>
2025-03-20 00:48:08 +0300

    Добавлены файлы Docker для проекта

--- djangotutorial/polls/restframeworkurls.py
+++ djangotutorial/polls/restframeworkurls.py
@@ -1,51 +1,38 @@
-from django.urls import path  # Для определения URL-маршрутов
-from django.urls import include  # Для включения других маршрутов
-from rest_framework import routers, serializers, viewsets  # Для работы с API
-from .models import User, Measurement  # Импортируем модели из текущего приложения
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers, serializers, viewsets
+from .models import User, Measurement
 
 # Сериализатор для модели User
-# Сериализатор преобразует данные модели в формат JSON и обратно
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = User  # Указываем модель, которую сериализуем
-        fields = ['url', 'first_name', 'last_name', 'email', 'registration_date']  # Поля, которые будут доступны через API
+        model = User
+        fields = '__all__'  # Сериализуем все поля модели
 
 # Сериализатор для модели Measurement
-# Аналогично UserSerializer, но для модели Measurement
 class MeasurementSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Measurement  # Указываем модель Measurement
-        fields = [
-            'url',
-            'user',  # Связь с пользователем (ForeignKey)
-            'waist_circumference',  # Обхват талии
-            'chest_circumference',  # Обхват груди
-            'torso_length',  # Длина торса
-            'hip_circumference',  # Обхват бедер
-            'leg_length',  # Длина ноги
-            'sleeve_length'  # Длина рукава
-        ]  # Поля, которые будут доступны через API
+        model = Measurement
+        fields = '__all__'  # Сериализуем все поля модели
 
 # ViewSet для модели User
-# ViewSet предоставляет CRUD-операции для модели
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()  # Определяем набор данных (все объекты модели User)
-    serializer_class = UserSerializer  # Указываем сериализатор для преобразования данных
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 # ViewSet для модели Measurement
-# Аналогично UserViewSet, но для модели Measurement
 class MeasurementViewSet(viewsets.ModelViewSet):
-    queryset = Measurement.objects.all()  # Определяем набор данных (все объекты модели Measurement)
-    serializer_class = MeasurementSerializer  # Указываем сериализатор для преобразования данных
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
 
-# Роутер для автоматической генерации URL
-# DefaultRouter автоматически создает URL-маршруты для ViewSet'ов
+# Создаем роутер и регистрируем ViewSet
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)  # Регистрируем маршрут для UserViewSet
-router.register(r'measurements', MeasurementViewSet)  # Регистрируем маршрут для MeasurementViewSet
+router.register(r'users', UserViewSet)  # Регистрируем ViewSet для User
+router.register(r'measurements', MeasurementViewSet)  # Регистрируем ViewSet для Measurement
 
-# Подключаем маршруты API
+# Подключаем маршруты роутера и добавляем URL для авторизации
 urlpatterns = [
-    path('', include(router.urls)),  # Включаем маршруты, сгенерированные роутером
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))  # Добавляем маршруты для аутентификации DRF
+    path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),  # Авторизация
+    path('', include(router.urls)),  # Маршруты API
 ]
